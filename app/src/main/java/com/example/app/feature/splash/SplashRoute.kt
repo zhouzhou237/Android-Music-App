c17b2a7f74caa2839f26ce29f3c841854f5b0eef
zhouzhou237 <82035040+zhouzhou237@users.noreply.github.com>
2025-01-03 21:11:53 +0100

    Second command - Unify color and Enriched login screen

--- app/src/main/java/com/example/app/feature/splash/SplashRoute.kt
+++ app/src/main/java/com/example/app/feature/splash/SplashRoute.kt
@@ -1,21 +1,23 @@
 package com.example.app.feature.splash
 
-import android.window.SplashScreen
-import androidx.annotation.IntRange
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.runtime.Composable
+import androidx.compose.material3.Text
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.app.R
 import com.example.app.ui.theme.AppTheme
+import androidx.compose.ui.graphics.Color
+import androidx.compose.material3.lightColorScheme
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.runtime.Composable
 
 @Composable
 fun SplashRoute() {
@@ -44,7 +46,18 @@ fun SplashScreen() {
                 .align(Alignment.BottomCenter)
         )
         //endregion
+
+        //版权文件
+        Text(text = stringResource(id = R.string.copyright),
+            color = MaterialTheme.colorScheme.onPrimary,
+            style = MaterialTheme.typography.bodySmall,
+            modifier = Modifier.align(Alignment.BottomCenter)
+                .padding(bottom = 30.dp)
+        )
+
     }
+
+
 }
 
 @Preview(showBackground = true)
--- app/src/main/java/com/example/app/ui/theme/Color.kt
+++ app/src/main/java/com/example/app/ui/theme/Color.kt
@@ -2,10 +2,95 @@ package com.example.app.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Purple80 = Color(0xFFD0BCFF)
-val PurpleGrey80 = Color(0xFFCCC2DC)
-val Pink80 = Color(0xFFEFB8C8)
+val md_theme_light_primary = Color(0xFFff3a3a)
+val md_theme_light_primary30 = Color(0xFFff3a3a)
+val md_theme_light_onPrimary = Color(0xFFFFFFFF)
+val md_theme_light_primaryContainer = Color(0xFFff3a3a) //FloatingActionButton背景
+val md_theme_light_onPrimaryContainer = Color(0xFFFFFFFF)//FloatingActionButton前景
+val md_theme_light_secondary = Color(0x4CFFFFFF) //HorizontalPager指示器默认颜色
+val md_theme_light_onSecondary = Color(0xFF0000FF)
+val md_theme_light_secondaryContainer = Color(0xff00ff00) //底部导航选中背景颜色
+val md_theme_light_onSecondaryContainer = Color(0xFF2C1513)
+val md_theme_light_tertiary = Color(0xFF725B2E)
+val md_theme_light_onTertiary = Color(0xFFFF0000)
+val md_theme_light_tertiaryContainer = Color(0xFFFFDEA6)
+val md_theme_light_onTertiaryContainer = Color(0xFF271900)
+val md_theme_light_error = Color(0xFFBA1A1A)
+val md_theme_light_errorContainer = Color(0xFFF0F0F0)
+val md_theme_light_onError = Color(0xFFFFFFFF)
+val md_theme_light_onErrorContainer = Color(0xFF410002)
+val md_theme_light_background = Color(0xFFF6F6F6) //背景
+val md_theme_light_onBackground = Color(0xFF1a1a1a)
+val md_theme_light_surface = Color(0XFFFFFFFF) // 状态栏，TopAppBar背景，NavigationBar背景
+val md_theme_light_onSurface = Color(0xFF201A1A)
+val md_theme_light_surfaceVariant = Color(0xFFFFFFFF) //card背景
+val md_theme_light_onSurfaceVariant = Color(0xFF534342)
+val md_theme_light_outline = Color(0x99000000) //次要文本颜色
+val md_theme_light_inverseOnSurface = Color(0xFFFF0000)
+val md_theme_light_inverseSurface = Color(0xFF362F2E)
+val md_theme_light_inversePrimary = Color(0xFF257896)
+val md_theme_light_shadow = Color(0xFF00FF00)
+val md_theme_light_surfaceTint = Color(0xFFFF0000)
+val md_theme_light_outlineVariant = Color(0xFFf5f5f5) //TabRow,indicator默认横线
+val md_theme_light_scrim = Color(0xFF00FF00) //ModalBottomSheet弹窗半透明背景
 
-val Purple40 = Color(0xFF6650a4)
-val PurpleGrey40 = Color(0xFF625b71)
-val Pink40 = Color(0xFF7D5260)
+//分割线颜色
+val md_theme_light_divider = Color(0xFFf5f5f5)
+
+//箭头颜色，例如：查看更多右侧箭头
+val md_theme_light_arrow = Color(0xffcdcdcd)
+
+//歌单详情分享等按钮默认背景颜色
+val md_theme_light_transparent_button_bg = Color(0x33ffffff)
+
+val md_theme_light_button_bg = Color(0x33ffffff)
+val md_theme_light_user_info_second = Color(0xaaffffff)
+
+val md_theme_dark_primary = Color(0xFFff3a3a)
+val md_theme_dark_onPrimary = Color(0xFFFFFFFF)
+val md_theme_dark_primaryContainer = Color(0xFFff3a3a)
+val md_theme_dark_onPrimaryContainer = Color(0xFFff3a3a)
+val md_theme_dark_secondary = Color(0xFFf04142)
+val md_theme_dark_onSecondary = Color(0xFF442927)
+val md_theme_dark_secondaryContainer = Color(0xFF5D3F3D)
+val md_theme_dark_onSecondaryContainer = Color(0xFFFFDAD7)
+val md_theme_dark_tertiary = Color(0xFFE1C28C)
+val md_theme_dark_onTertiary = Color(0xFF402D04)
+val md_theme_dark_tertiaryContainer = Color(0xFF594319)
+val md_theme_dark_onTertiaryContainer = Color(0xFFFFDEA6)
+val md_theme_dark_error = Color(0xFFFFB4AB)
+val md_theme_dark_errorContainer = Color(0xFF93000A)
+val md_theme_dark_onError = Color(0xFF690005)
+val md_theme_dark_onErrorContainer = Color(0xFFFFDAD6)
+val md_theme_dark_background = Color(0xff111111)
+val md_theme_dark_onBackground = Color(0xFFEDE0DE)
+val md_theme_dark_surface = Color(0xFF201A1A)
+val md_theme_dark_onSurface = Color(0xFFEDE0DE)
+val md_theme_dark_surfaceVariant = Color(0xff181818)
+val md_theme_dark_onSurfaceVariant = Color(0xFFD8C2BF)
+val md_theme_dark_outline = Color(0xFFA08C8A)
+val md_theme_dark_inverseOnSurface = Color(0xFF201A1A)
+val md_theme_dark_inverseSurface = Color(0xFFEDE0DE)
+val md_theme_dark_inversePrimary = Color(0xFFBB1622)
+val md_theme_dark_shadow = Color(0xFF000000)
+val md_theme_dark_surfaceTint = Color(0xFFFFB3AD)
+val md_theme_dark_outlineVariant = Color(0xFF534342)
+val md_theme_dark_scrim = Color(0xFF000000)
+
+//分割线颜色
+val md_theme_dark_divider = Color(0xff252525)
+
+//箭头颜色，例如：查看更多右侧箭头
+val md_theme_dark_arrow = Color(0xff606060)
+
+//主色调链接按钮颜色，类似微信用户详情按钮
+val md_theme_second_button_link = Color(0xFF596c94)
+val md_theme_transparent_88 = Color(0x88888888)
+
+//vip金色
+val md_theme_vip = Color(0xFFFFDBA6)
+val md_theme_vip_background = Color(0xFF555555)
+
+val button_transparent_88 = Color(0x88000000)
+
+val seed = Color(0xFFff3a3a)
